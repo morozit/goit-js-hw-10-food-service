f8740cc0f15118ab64d106546e2432390f90ee47
Roman <65686226+morozit@users.noreply.github.com>
2020-11-17 01:47:53 +0200

    Create variables.js

---
+++ src/JS/variables.js
@@ -0,0 +1,11 @@
+// export const refs = {
+//   menuList: document.querySelector('.js-menu'),
+//   checkbox: document.querySelector('#theme-switch-toggle'),
+//   body: document.querySelector('body'),
+//   savedTheme: localStorage.getItem('Theme'),
+// };
+
+// export const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// };
